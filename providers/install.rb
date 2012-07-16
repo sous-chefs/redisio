bd6b7d33a109ef4ffb37f1d6c826f4d1913360b9
brianbianco <bbianco@cargometrics.com>
2012-07-16 11:41:26 -0400

    Updated the install provider with the ability to inherit updated_by_last_action from the resources nested inside of it

--- providers/install.rb
+++ providers/install.rb
@@ -66,70 +66,93 @@ def configure
     #Merge the configuration defaults with the provided array of configurations provided
     current = current_defaults_hash.merge(current_instance_hash)
 
-    #Create the owner of the redis data directory
-    user current['user'] do
-      comment "Redis service account"
-      supports :manage_home => true
-      home current['homedir']
-      shell current['shell']
+    #Setup a sub-run-context
+    sub_run_context = @run_context.dup
+    sub_run_context.resource_collection = Chef::ResourceCollection.new
+
+    begin
+      #Set the run_context equal to the sub_run_context that has had its resource collection emptied.
+      original_run_context, @run_context = @run_context, sub_run_context
+
+      #Create the owner of the redis data directory
+      user current['user'] do
+        comment "Redis service account"
+        supports :manage_home => true
+        home current['homedir']
+        shell current['shell']
+      end
+
+      #Create the redis configuration directory
+      directory current['configdir'] do
+        owner 'root'
+        group 'root'
+        mode "0755"
+        action :create
+      end
+      #Create the instance data directory
+      directory current['datadir'] do
+        owner current['user']
+        group current['group']
+        mode "0755"
+        action :create
+      end
+      template "#{current['configdir']}/#{current['port']}.conf" do
+        source 'redis.conf.erb'
+        owner current['user']
+        group current['group']
+        mode '0644'
+        variables({
+          :port                   => current['port'],
+          :address                => current['address'],
+          :databases              => current['databases'],
+          :backuptype             => current['backuptype'],
+          :datadir                => current['datadir'],
+          :timeout                => current['timeout'],
+          :loglevel               => current['loglevel'],
+          :save                   => current['save'],
+          :slaveof                => current['slaveof'],
+          :masterauth             => current['masterauth'],
+          :slaveservestaledata    => current['slaveservestaledata'], 
+          :replpingslaveperiod    => current['replpingslaveperiod'],
+          :repltimeout            => current['repltimeout'],
+          :requirepass            => current['requirepass'],
+          :maxclients             => current['maxclients'],
+          :maxmemory              => current['maxmemory'],
+          :maxmemorypolicy        => current['maxmemorypolicy'],
+          :maxmemorysamples       => current['maxmemorysamples'],
+          :appendfsync            => current['appendfsync'],
+          :noappendfsynconrewrite => current['noappendfsynconrewrite'],
+          :aofrewritepercentage   => current['aofrewritepercentage'] ,
+          :aofrewriteminsize      => current['aofrewriteminsize'],
+          :includes               => current['includes']
+        })
+      end
+      #Setup init.d file
+      template "/etc/init.d/redis#{current['port']}" do
+        source 'redis.init.erb'
+        owner 'root'
+        group 'root'
+        mode '0755'
+        variables({
+          :port => current['port']
+        })
+      end
+    ensure      
+      #now that we are done with the sub run, set the run context back to the original
+      @run_context = original_run_context
+    end # sub-run-context block end
+
+    # Here we check the resource collection of the sub run to see if anything was updated.  If they were, we called updated by last action on our resource
+    begin
+      # Converge the sub run, since it is nested it will be converged when the regular run_context is being converged.
+      Chef::Runner.new(sub_run_context).converge
+    ensure
+      if sub_run_context.resource_collection.any?(&:updated?)
+        new_resource.updated_by_last_action(true)
+      end
     end
 
-    #Create the redis configuration directory
-    directory current['configdir'] do
-      owner 'root'
-      group 'root'
-      mode "0755"
-      action :create
-    end
-    #Create the instance data directory
-    directory current['datadir'] do
-      owner current['user']
-      group current['group']
-      mode "0755"
-      action :create
-    end
-    template "#{current['configdir']}/#{current['port']}.conf" do
-      source 'redis.conf.erb'
-      owner current['user']
-      group current['group']
-      mode '0644'
-      variables({
-        :port                   => current['port'],
-        :address                => current['address'],
-        :databases              => current['databases'],
-        :backuptype             => current['backuptype'],
-        :datadir                => current['datadir'],
-        :timeout                => current['timeout'],
-        :loglevel               => current['loglevel'],
-        :save                   => current['save'],
-        :slaveof                => current['slaveof'],
-        :masterauth             => current['masterauth'],
-        :slaveservestaledata    => current['slaveservestaledata'], 
-        :replpingslaveperiod    => current['replpingslaveperiod'],
-        :repltimeout            => current['repltimeout'],
-        :requirepass            => current['requirepass'],
-        :maxclients             => current['maxclients'],
-        :maxmemory              => current['maxmemory'],
-        :maxmemorypolicy        => current['maxmemorypolicy'],
-        :maxmemorysamples       => current['maxmemorysamples'],
-        :appendfsync            => current['appendfsync'],
-        :noappendfsynconrewrite => current['noappendfsynconrewrite'],
-        :aofrewritepercentage   => current['aofrewritepercentage'] ,
-        :aofrewriteminsize      => current['aofrewriteminsize'],
-        :includes               => current['includes']
-     })
-    end
-    #Setup init.d file
-    template "/etc/init.d/redis#{current['port']}" do
-      source 'redis.init.erb'
-      owner 'root'
-      group 'root'
-      mode '0755'
-      variables({
-        :port => current['port']
-      })
-    end
-  end
+  end # servers each loop
 end
 
 def redis_exists?
