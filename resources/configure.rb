34f6196debbad9951933e6d8722aa282cc7286de
Ryan Cleere <ryan.cleere@rackspace.com>
2013-06-24 18:11:38 -0500

    Split the install LWRP and recipe into install and configure LWRPs and recipes

--- resources/configure.rb
+++ resources/configure.rb
@@ -1,8 +1,9 @@
 #
 # Cookbook Name:: redisio
-# Resource::install
+# Resource::configure
 #
 # Copyright 2013, Brian Bianco <brian.bianco@gmail.com>
+# Copyright 2013, Rackspace Hosting <ryan.cleere@rackspace.com>
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -21,16 +22,14 @@ actions :run
 #Uncomment this and remove the block in initialize when ready to drop support for chef <= 0.10.8
 #default_action :run
 
-#Installation attributes
+#Configuration attributes
 attribute :version, :kind_of => String
-attribute :download_url, :kind_of => String
-attribute :download_dir, :kind_of => String, :default => Chef::Config[:file_cache_path]
-attribute :artifact_type, :kind_of => String, :default => 'tar.gz'
-attribute :base_name, :kind_of => String, :default => 'redis-'
-attribute :safe_install, :kind_of => [ TrueClass, FalseClass ], :default => true
 attribute :base_piddir, :kind_of => String, :default => '/var/run/redis'
+attribute :user, :kind_of => String, :default => 'redis'
+attribute :group, :kind_of => String, :default => 'redis'
 
-attribute :install_dir, :kind_of => String, :default => nil
+attribute :default_settings, :kind_of => Hash
+attribute :servers, :kind_of => Array
 
 def initialize(name, run_context=nil)
   super
