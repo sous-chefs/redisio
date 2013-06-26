f42e183b3bacffe0b5e2af35e4f7c5cfa6911506
Ryan Cleere <ryan.cleere@rackspace.com>
2013-06-26 13:20:13 -0500

    Removed base_piddir since it was moved to configured

--- resources/install.rb
+++ resources/install.rb
@@ -28,7 +28,6 @@ attribute :download_dir, :kind_of => String, :default => Chef::Config[:file_cach
 attribute :artifact_type, :kind_of => String, :default => 'tar.gz'
 attribute :base_name, :kind_of => String, :default => 'redis-'
 attribute :safe_install, :kind_of => [ TrueClass, FalseClass ], :default => true
-attribute :base_piddir, :kind_of => String, :default => '/var/run/redis'
 
 attribute :install_dir, :kind_of => String, :default => nil
 
