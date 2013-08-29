b743be71f0b6b9f703f9836fd1d804958d021909
Denis Barishev <denz@twiket.com>
2013-08-30 02:41:02 +0700

    removed version logging, since version method might be invoked more than once

--- providers/install.rb
+++ providers/install.rb
@@ -279,7 +279,6 @@ def version
     redis_version = Mixlib::ShellOut.new("#{redis_server} -v")
     redis_version.run_command
     version = redis_version.stdout[/version (\d*.\d*.\d*)/,1] || redis_version.stdout[/v=(\d*.\d*.\d*)/,1]
-    Chef::Log.info("The Redis server version is: #{version}")
     return version.gsub("\n",'')
   end
   nil
