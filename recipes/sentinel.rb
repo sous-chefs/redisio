83fa356c2294ce1d332bfa25a5f80a037390d7ae
Martin Smith <martin@mbs3.org>
2014-11-19 15:50:42 -0600

    Use correct parameter 'sentinel_port' on defaults
    Fixes #157, use 'sentinel_port' not 'port' for defining sentinels.

--- recipes/sentinel.rb
+++ recipes/sentinel.rb
@@ -25,7 +25,7 @@ redis = node['redisio']
 
 sentinel_instances = redis['sentinels']
 if sentinel_instances.empty?
-  sentinel_instances = [{'port' => '26379', 'name' => 'mycluster', 'master_ip' => '127.0.0.1', 'master_port' => 6379}]
+  sentinel_instances = [{'sentinel_port' => '26379', 'name' => 'mycluster', 'master_ip' => '127.0.0.1', 'master_port' => '6379'}]
 end
 
 redisio_sentinel "redis-sentinels" do
