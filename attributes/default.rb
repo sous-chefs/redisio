8f82639ce716b7859b11f90cb3e1a5f36e9ca621
Frits Brusse <fbrusse@schubergphilis.com>
2015-03-23 17:10:32 +0100

    make template configurable

--- attributes/default.rb
+++ attributes/default.rb
@@ -42,6 +42,10 @@ else
   package_name = 'redis'
 end
 
+# Overwite template
+default['redisio']['template_cookbook'] = 'redisio'
+default['redisio']['template_source'] = 'redis.conf.erb'
+
 # Install related attributes
 default['redisio']['safe_install'] = true
 default['redisio']['package_install'] = default_package_install
